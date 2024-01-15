3b647ee8bafeec02697a3012b09becbe0f49d237
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2024-01-15 16:45:16 -0300

    fix(task): fix validate fields

--- api/app/Http/Requests/StoreTaskRequest.php
+++ api/app/Http/Requests/StoreTaskRequest.php
@@ -4,20 +4,23 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Traits\FailedValidationTrait;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreTaskRequest extends FormRequest
 {
+    use FailedValidationTrait;
+
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     public function rules(): array
     {
         return [
             'title' => 'required|string|min:5|max:255',
-            'description' => 'required|text|min:8|max:1024',
+            'description' => 'required|string|min:8|max:1024',
             'user_id' => 'required|numeric|exists:users,id',
         ];
     }
