0361a8729914fa66d6ea13413826ed2821e56c0b
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2024-01-15 16:12:20 -0300

    feat(task): define routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-
+    Route::resource('tasks', \App\Http\Controllers\TaskController::class);
 });
