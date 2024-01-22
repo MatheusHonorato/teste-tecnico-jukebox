37d99af5b52b49fcae4384798621623f29dbea2a
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2024-01-22 12:44:42 -0300

    feat(app) set patterns

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,13 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
 
-Route::post('setToken', [\App\Http\Controllers\FirebasePushController::class, 'setToken'])->name('firebase.token');
+Route::post('tokens/{user}', [\App\Http\Controllers\FirebasePushController::class, 'setToken'])->name('firebase.token');
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::post('send/notification', [\App\Http\Controllers\FirebasePushController::class,'notification'])->name('firebase.send');
+    Route::post('notification', [\App\Http\Controllers\FirebasePushController::class,'notification'])->name('firebase.notification');
 
     Route::resource('tasks', \App\Http\Controllers\TaskController::class);
 });
