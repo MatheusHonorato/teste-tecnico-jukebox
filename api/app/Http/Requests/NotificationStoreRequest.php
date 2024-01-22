f8368ffcf89fd0373d5622ff500458df0ee2ecb8
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2024-01-22 12:41:35 -0300

    feat(app) add specify request

---
+++ api/app/Http/Requests/NotificationStoreRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Traits\FailedValidationTrait;
+use Illuminate\Foundation\Http\FormRequest;
+
+class NotificationStoreRequest extends FormRequest
+{
+    use FailedValidationTrait;
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+            'body' => 'required|string'
+        ];
+    }
+
+}
