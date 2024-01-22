c0eb931eb6359561cac589ad491750e2f5e61f46
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2024-01-22 12:42:43 -0300

    feat(app) clear comments

--- api/app/Http/Resources/TaskResourceCollection.php
+++ api/app/Http/Resources/TaskResourceCollection.php
@@ -9,11 +9,6 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class TaskResourceCollection extends ResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @return array<int|string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
