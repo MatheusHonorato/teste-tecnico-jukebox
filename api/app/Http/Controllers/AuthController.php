f8bb8669e392075af7c1e90db2e9aa0d42ca3324
Matheus <matheus@LAPTOP-5QQPHSHE.localdomain>
2024-01-29 11:59:54 -0300

    feat(auth) set return type

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -49,11 +49,11 @@ class AuthController
      * )
      *
      */
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         try {
 
-            // Ajuste necessário pois existe uma pequena diferença entre o timezone do servidor e da api gerando problemas ao verificar o token no back-end.
+            // Ajuste necessário, pois existe uma pequena diferença entre o timezone do servidor e da api gerando problemas ao verificar o token no back-end.
             // Tentei alterar configurações de timezone no painel do firebase e na aplicação, mas não obtive sucesso.
             $this->delayTimeZoneDiference();
 
